85ea127909597ef57fce41e3cf3a0a24dbb14c7c
khalid saaf <khallid.saaf@gmail.com>
2025-11-10 00:08:23 +0100

    fin parti html

--- login-sugnup/src/App.js
+++ login-sugnup/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
 
 function App() {
   return (
     <div >
-     
+     <LoginSignup/>
     </div>
   );
 }
--- login-sugnup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-sugnup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import "./LoginSignup.css";
+import user_icon from "../Assets/icon-user.png";
+import mail_icon from "../Assets/icon-mail.png";
+import password_icon from "../Assets/icon-password.png";
+
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className="container">
+      <div className="header">
+        <div className="text">Signup</div>
+        <div className="underlaine"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src={mail_icon} alt="" />
+          <input type="mail" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" />
+        </div>
+      </div>
+      <div className="forgot-passwor">Lost Password ? <span>Click Here !</span></div>
+      <div className="submit-container">
+        <div className="submit">Sign up</div>
+        <div className="submit">Login</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
-
-
+export default LoginSignup;
